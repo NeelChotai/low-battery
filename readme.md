f2141135e53105f0720ed0959dca6a5ce3513fdd
Neel <neel@chot.ai>
2020-01-20 15:52:45 +0000

    added instructions, verbosity, command line arguments
    update readme
    
    whitespace

--- readme.md
+++ readme.md
@@ -1,14 +1,11 @@
 # Low battery monitor
 
-A simple battery monitor written in Rust. Notifications are sent at 10% and 5%.
+A simple battery monitor written in Rust. Called with arguments to specifiy at which battery percentages notifications should be sent:
 
-Requires a notification daemon such as Dunst.
-
-Example cron job:
 ```
-* * * * * export DISPLAY=:0 && /opt/low-battery-monitor
+low-battery -l 10 -c 5
 ```
 
-Todo:
-- Prevent notification from being displayed if one has already been displayed within the last x minutes.
-- Show battery percentage in notification.
+Where `l` is the first (low) notification and `c` is the second (critical). 
+
+Scans every 2 minutes unless battery is above 30% where the interval is increased to 10 minutes.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,61 @@
-use std::fs;
-use std::process::Command;
-
-const NOTIF_HIGH: i32 = 10;
-const NOTIF_LOW: i32 = 5;
+use std::{fs, process, thread, time};
+use notify_rust::Notification;
+use clap::{Arg, App};
 
 fn main() {
-    let file = fs::read_to_string("/sys/class/power_supply/BAT0/capacity")
-                .expect("I/O error.");
-    let level: i32 = file.trim().parse::<i32>().unwrap();
-    let status = fs::read_to_string("/sys/class/power_supply/BAT0/status")
-                .expect("I/O error.");
+    let args = App::new("Low battery monitor")
+        .version("0.2.0")
+        .author("Neel Chotai <neel@chot.ai>")
+        .about("Monitors battery and notifies at specified levels.")
+        .arg(Arg::with_name("low")
+            .short("l")
+            .long("low")
+            .value_name("LEVEL")
+            .help("First level to notify at.")
+            .required(true))
+        .arg(Arg::with_name("critical")
+            .short("c")
+            .long("critical")
+            .value_name("LEVEL")
+            .help("Last level to notify at.")
+            .required(true))
+        .get_matches();
+
+    let low: i32 = args.value_of("low").unwrap_or("10").parse::<i32>().unwrap_or(10);
+    let crit: i32 = args.value_of("critical").unwrap_or("5").parse::<i32>().unwrap_or(5);
+
+    if low <= crit {
+        eprintln!("Critical value is greater than low value! Exiting...");
+        process::exit(0);
+    }
+
+    loop {
+        let file = fs::read_to_string("/sys/class/power_supply/BAT0/capacity")
+            .expect("I/O error.");
+        let level: i32 = file.trim().parse::<i32>().unwrap();
+        let status = fs::read_to_string("/sys/class/power_supply/BAT0/status")
+            .expect("I/O error.");
     
-    if status.trim() == "Discharging" {
-        if level <= NOTIF_LOW {
-            Command::new("notify-send")
-                    .arg("Battery level is critical.")
-                    .spawn()
+        if status.trim() == "Discharging" {
+            if level <= crit {
+                Notification::new()
+                    .body(&format!("Battery level is critical ({}%).", level))
+                    .show()
                     .expect("Failed to notify.");
+            }
+            else if level <= low {
+                Notification::new()
+                    .body(&format!("Battery level is low ({}%).", level))
+                    .show()
+                    .expect("Failed to notify.");
+            }
         }
-        else if level <= NOTIF_HIGH {
-            Command::new("notify-send")
-                    .arg("Battery level is low.")
-                    .spawn()
-                    .expect("Failed to notify.");
+
+        if level > 30 {
+            thread::sleep(time::Duration::from_secs(600));
+        }
+        else {
+            thread::sleep(time::Duration::from_secs(120));
         }
     }
 }
